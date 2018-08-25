85fbdb4a9ebfeaa680151f8f513c8eac1c8019e8
KornFlakesGuy <fer.aravalo1997@gmail.com>
2018-08-25 09:52:33 -0600

    Subida de proyecto.

---
+++ production/footer.php
@@ -0,0 +1,6 @@
+        <footer>
+          <div class="pull-rigth">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
